04b6da6d54b48de0030a9a4d32e474c7f21f5ebe
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-12-09 23:52:53 -0300

    create service_test.go and main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"ChallengeBackEndPP/configs"
+	"ChallengeBackEndPP/internal/handler"
+	"ChallengeBackEndPP/internal/repository"
+	"ChallengeBackEndPP/user"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"net/http"
+)
 
+func main() {
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := configs.LoadDatabase()
+	if err != nil {
+		panic(err)
+	}
+
+	userDb := repository.NewUser(db)
+	userService := user.NewService(userDb)
+	userHandler := handler.NewUserHandler(userService)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/user", func(r chi.Router) {
+		r.Post("/", userHandler.CreateUser)
+	})
+
+	http.ListenAndServe(":8000", r)
 }
