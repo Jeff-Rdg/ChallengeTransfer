e1a859cda2f04dc341941a13fb4867dd7ee29e44
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-12-11 22:36:13 -0300

    add routes.go and walletHandler.go

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"ChallengeBackEndPP/internal/handlers"
 	"ChallengeBackEndPP/internal/repository"
 	"ChallengeBackEndPP/user"
+	"ChallengeBackEndPP/wallet"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"gorm.io/gorm"
@@ -14,6 +15,10 @@ func LoadRoutes(db *gorm.DB) *chi.Mux {
 	userService := user.NewService(userDb)
 	userHandler := handlers.NewUserHandler(userService)
 
+	walletDb := repository.NewWallet(db)
+	walletService := wallet.NewService(walletDb)
+	walletHandler := handlers.NewWalletHandler(walletService)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
@@ -21,6 +26,11 @@ func LoadRoutes(db *gorm.DB) *chi.Mux {
 	r.Route("/user", func(r chi.Router) {
 		r.Post("/", userHandler.CreateUser)
 		r.Get("/{id}", userHandler.FindUserById)
+	})
+
+	r.Route("/wallet", func(r chi.Router) {
+		r.Post("/", walletHandler.CreateWallet)
+		r.Get("/{id}", walletHandler.FindWalletById)
 	})
 
 	return r
