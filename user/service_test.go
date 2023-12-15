be269f232adb4863d8ceac993495f4504d5c8b0f
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-12-14 22:41:32 -0300

    feat test

--- user/service_test.go
+++ user/service_test.go
@@ -1,1 +1,39 @@
 package user
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestService_Create(t *testing.T) {
+	var service Service
+
+	type testCase struct {
+		testName    string
+		request     Request
+		expectedErr error
+	}
+
+	testCases := []testCase{
+		{
+			testName: "invalid name",
+			request: Request{
+				FullName:     "name with number 1",
+				TaxNumber:    "933.203.710-88",
+				Email:        "test@gmail.com",
+				Password:     "123456",
+				IsShopkeeper: false,
+			},
+			expectedErr: FullNameInvalidErr,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			_, err := service.Create(tc.request)
+			if !errors.Is(err, tc.expectedErr) {
+				t.Errorf("expected error %v, got %v", tc.expectedErr, err)
+			}
+		})
+	}
+}
